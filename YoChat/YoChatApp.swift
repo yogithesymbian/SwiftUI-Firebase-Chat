dac435ca9870bd5d492384e6f864ae7739fb2e72
yogithesymbian <yogirenbox33@gmail.com>
2023-08-01 10:02:02 +0800

    Initial Commit

---
+++ YoChat/YoChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoChatApp.swift
+//  YoChat
+//
+//  Created by Yogi Arif Widodo on 01/08/23.
+//
+
+import SwiftUI
+
+@main
+struct YoChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
